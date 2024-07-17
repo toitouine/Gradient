07b29301d5775612152a82e01f97d13e1d13e6fd
toitouine <antoine.mechulam@gmail.com>
2024-07-17 15:43:38 +0200

    Adding default save folder verification

--- src/Config.java
+++ src/Config.java
@@ -1,6 +1,19 @@
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
 public class Config {
   private Config() {}
 
   // Chemin (absolu) vers le dossier de sauvegarde par défaut
-  public final static String defaultSaveDirectory = "";
+  public static String defaultSaveDirectory = "";
+
+  static {
+    // Vérifie que le chemin est valide
+    if (!defaultSaveDirectory.equals("")) {
+      if (!Files.exists(Paths.get(defaultSaveDirectory))) {
+        defaultSaveDirectory = "";
+        System.out.println("Fichier de sauvegarde par défaut invalide !");
+      }
+    }
+  }
 }
